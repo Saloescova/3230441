18e8168d563691241517f9b44a99f8847eafc05b
Saloescova <saloescobarvallejo@gmail.com>
2025-06-05 15:33:04 -0500

    Creación proyecto Par e Impar

---
+++ ConceptosProgramacion/NumeroParimpar/Program.cs
@@ -0,0 +1,14 @@
+﻿do
+{
+    Console.Write("Ingrese un numero:");
+    var numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("El numero es Par");
+    }
+    else
+    {
+        Console.WriteLine("El numero es impar ");
+    }
+}while (true);
